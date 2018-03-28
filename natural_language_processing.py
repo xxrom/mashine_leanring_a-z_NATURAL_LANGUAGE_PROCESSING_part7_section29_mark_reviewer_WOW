614f8dde9ef0a984e922a72c88b2d51a596cc914
Nikita Chernyshov <xpom55@mail.ru>
2018-03-28 19:39:55 +0300

    we will apply Naive Bayes classification for our dataset and get 73% of accuracy

--- natural_language_processing.py
+++ natural_language_processing.py
@@ -44,12 +44,36 @@ for i in range(0, 1000) :
 
 # Creating the Bag of Words model
 from sklearn.feature_extraction.text import CountVectorizer
-# также размер можно уменьшить из 9 раздела dimensionality reduction
+# помимо max_features, размерность можно уменьшить 9 dimensionality reduction
 cv = CountVectorizer(max_features = 1500) # max columns of features 1500
 X = cv.fit_transform(corpus).toarray() # fit_transform ???
 y = dataset.iloc[:, 1].values
 
+# для классификации в NLP обычно используют NaiveBayes, Descicion Tree C, RFC
 
+# Naive Bayes
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+# Feature Scaling не нужно, так как мы в большей степени имеем 0
+# совсем немного 1 и еще меньше 2, 3 и 4, поэтому не имеет смысл применять
+# from sklearn.preprocessing import StandardScaler
+# sc_X = StandardScaler()
+# X_train = sc_X.fit_transform(X_train)
+# X_test = sc_X.transform(X_test)
+
+# Fitting the classifire to the Training set
+from sklearn.naive_bayes import GaussianNB
+classifier = GaussianNB()
+classifier.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = classifier.predict(X_test) # предсказываем данные из X_test
+
+# Making the Confusion Matrix # узнаем насколько правильная модель
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred) # in console cm
 
 
 
